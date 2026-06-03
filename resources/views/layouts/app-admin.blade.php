6f8211759af11bbb08b06e893c4da4e0def1cd88
Kartika Tri Juliana <kartikatjuliana@gmail.com>
2026-06-03 15:07:52 +0700

    hapus edit profil

--- resources/views/layouts/app-admin.blade.php
+++ resources/views/layouts/app-admin.blade.php
@@ -182,18 +182,6 @@
                 <div id="profileDropdown"
                     class="hidden absolute right-0 mt-2 w-48 bg-white rounded-xl shadow-xl overflow-hidden z-50">
 
-                    <a href="#"
-                    class="flex items-center gap-3 px-4 py-3 text-sm text-gray-700 hover:bg-gray-100">
-
-                        <svg class="w-4 h-4 text-primary"
-                            fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M5.121 17.804A9 9 0 1118.88 17.804M15 11a3 3 0 11-6 0a3 3 0 016 0z"/>
-                        </svg>
-
-                        Profile
-                    </a>
-
                     <a href="{{ route('logout') }}"
                         class="w-full flex items-center gap-3 px-4 py-3 text-sm text-red-600 hover:bg-red-50">
 
@@ -203,7 +191,7 @@
                                 d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a2 2 0 01-2 2H6a2 2 0 01-2-2V7a2 2 0 012-2h5a2 2 0 012 2v1"/>
                         </svg>
 
-                        Logout
+                        Keluar
                     </a>
 
                 </div>
